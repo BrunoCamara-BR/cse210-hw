d8abb1848c8e48a1540df35453919bad5f0dbb17
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-03-04 12:12:53 -0300

    w1 program

--- csharpDemo/Program.cs
+++ csharpDemo/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using System;
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
