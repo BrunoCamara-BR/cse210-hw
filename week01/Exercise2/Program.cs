036cfef8bd681fe07869845ee3a5df3615699f44
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-03-04 12:59:07 -0300

    w1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your score: ");
+        string userScore = Console.ReadLine();
+        float score = float.Parse(userScore);
+        string letter = "";
+        string msg = "";
+        string sign = "";
+
+        // The letter
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Sign
+        if (score % 10 >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (score % 10 <= 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        // Final message
+        if (score >= 70)
+        {
+            msg = "Congratulations you pass!";
+        }
+        else
+        {
+            msg = "Your score was not enough to pass, but it is normal, try again, you will get the missing score easily";
+        }
+
+        Console.WriteLine($"You got {sign}{letter}.");
+        Console.WriteLine(msg);
     }
 }
