7ada07e6aa2d05035cc34c58a832c7ee867cf2d0
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-03-04 22:17:13 -0300

    w1 exercise 5 - functions and methods

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int sqNumber = SquareNumber(userNumber);
+        DisplayResult(userName, sqNumber);
+    }
+    // DisplayWelcome - Displays the message, "Welcome to the Program!"
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    // PromptUserName - Asks for and returns the user's name (as a string)
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        Console.Write("Type your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+
+    // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int number)
+    {
+        return (int)Math.Pow(number, 2);
+    }
+
+    // DisplayResult - Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your program is {squareNumber}");
     }
 }
