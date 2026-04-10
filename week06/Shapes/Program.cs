d3d311889e8322138c56ce762879bff9b477ef75
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-04-09 21:57:01 -0300

    CSE week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("blue", 3);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("yellow", 2, 3);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("purple", 2);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("--------------------");
+        shapes.ForEach(item =>
+        {
+            Console.WriteLine(item.GetColor());
+            Console.WriteLine(item.GetArea());
+        });
     }
 }
